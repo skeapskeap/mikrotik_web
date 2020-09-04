96bf938d90538cb8fb373a2aede3adfb74820614
FIRST_NAME LAST_NAME <skeap@cn.ru>
2020-09-04 12:23:32 +0700

    hide secret key

--- dj_app/settings.py
+++ dj_app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from dj_app.secret_key import secret_key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'p@_3-!^5h5ghvlun01owy0hnb$9)ztgjjqjv@735!$3=zq2!qj'
+SECRET_KEY = secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
