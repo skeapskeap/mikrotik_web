cd92f2a0c2f2335626c8bbd3c7c46871ed3e1c1b
Ivan Tolstikhin <skeap@cn.ru>
2020-10-29 19:14:53 +0700

    default groups

--- register/admin.py
+++ register/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
 
-# Register your models here.
+default_groups = ['billing', 'view_only', 'net_admin']
+for group in default_groups:
+    Group.objects.update_or_create(name=group)
